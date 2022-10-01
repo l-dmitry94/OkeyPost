59d01d989ce2da62b21f00e8caf5584a2ddd4765
Liuba Dmytro <lyubadmitry2@gmail.com>
2022-10-01 16:51:02 +0300

    Start Project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$('.profit__tabs').owlCarousel({
+    loop:true,
+    nav:true,
+    items: 6,
+    dots: false,
+    margin: 8,
+    navText: ["<img src='img/arrow-left.svg'>","<img src='img/arrow-right.svg'>"]
+})
