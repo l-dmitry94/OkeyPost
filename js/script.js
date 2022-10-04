e2f1884135ad520795bb6a497e69ef7f85dc4a9c
Liuba Dmytro <lyubadmitry2@gmail.com>
2022-10-04 20:52:15 +0300

    Added profit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,34 @@
-$('.profit__tabs').owlCarousel({
-    loop:true,
-    nav:true,
-    items: 6,
-    dots: false,
-    margin: 8,
-    navText: ["<img src='img/arrow-left.svg'>","<img src='img/arrow-right.svg'>"]
+//Карусель табов
+$(function () {
+    $('.profit__tabs').owlCarousel({
+        loop: false,
+        nav: true,
+        items: 6,
+        dots: false,
+        margin: 8,
+        navText: ["<img src='img/arrow-left.svg'>", "<img src='img/arrow-right.svg'>"]
+    })
+})
+
+
+//Выбор таба
+$(function () {
+    $('.profit__tabs .tab').each(function () {
+        $(this).on('click', function () {
+            $('.profit__tabs .tab').removeClass('active');
+            $(this).addClass('active');
+        })
+    })
+})
+
+
+// Карусель продуктов
+$(function () {
+    $('.profit__products').owlCarousel({
+        loop: true,
+        nav: true,
+        items: 1,
+        dots: false,
+        navText: ["<div class='product-prev'><img src='img/jacket.png'></div><img src='img/arrow-left.svg'>", "<img src='img/arrow-right.svg'><div class='product-next'><img src='img/jacket.png'></div>"]
+    })
 })
